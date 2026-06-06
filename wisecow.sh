e76bef7266b96261ea2f2022d34aeb5d81066c5a
Harsh7209 <harshchoubay759@gmail.com>
2026-06-06 22:55:20 +0530

    reupdate the wisecow script

--- wisecow.sh
+++ wisecow.sh
@@ -9,21 +9,31 @@ get_api() {
 
 handleRequest() {
     get_api > /dev/null
-    local mod=$(fortune)
-    cat <<EOF
+	mod=`fortune`
+
+cat <<EOF
 HTTP/1.1 200 OK
 Content-Type: text/html
 Connection: close
 
-<pre>$(cowsay "$mod")</pre>
+<pre>`cowsay $mod`</pre>
 EOF
 }
 
+prerequisites() {
+	command -v cowsay >/dev/null 2>&1 &&
+	command -v fortune >/dev/null 2>&1 || 
+		{ 
+			echo "Install prerequisites."
+			exit 1
+		}
+}
+
 main() {
+	prerequisites
 	echo "Wisdom served on port=$SRVPORT..."
-	
 	while true; do
-		nc -lN -p $SRVPORT -q 1 | handleRequest
+		socat TCP-LISTEN:$SRVPORT,reuseaddr,fork EXEC:'handleRequest'
 	done
 }
 
