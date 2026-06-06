d8d9020024265a95ce0e85dfbdcc4f021e415c6f
Harsh7209 <harshchoubay759@gmail.com>
2026-06-06 23:27:26 +0530

    small fixes

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,12 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     bash \
     cowsay \
     fortune \
-    socat \
+    netcat-openbsd \
     && rm -rf /var/lib/apt/lists/*
 
 COPY wisecow.sh /usr/local/bin/wisecow.sh
-
-# FIX LINE ENDINGS WITH SED
 RUN sed -i 's/\r$//' /usr/local/bin/wisecow.sh && \
     chmod +x /usr/local/bin/wisecow.sh
 
 EXPOSE 4499
-
 ENTRYPOINT ["/usr/local/bin/wisecow.sh"]
--- wisecow.sh
+++ wisecow.sh
@@ -20,12 +20,21 @@ Connection: close
 EOF
 }
 
+prerequisites() {
+	command -v cowsay >/dev/null 2>&1 &&
+	command -v fortune >/dev/null 2>&1 || 
+		{ 
+			echo "Install prerequisites."
+			exit 1
+		}
+}
+
 main() {
-	echo "Wisdom served on http://localhost:$SRVPORT"
-	echo "Press Ctrl+C to stop"
+	prerequisites
+	echo "Wisdom served on port=$SRVPORT..."
 	
 	while true; do
-		socat TCP-LISTEN:$SRVPORT,reuseaddr,fork EXEC:'handleRequest'
+		nc -lN -p $SRVPORT | handleRequest
 	done
 }
 
